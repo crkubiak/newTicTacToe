a7f636db1c0f11ce62377f75627458dd4e0b50df
Charles Kubiak <crkubiak@Charless-MacBook-Pro.local>
2019-06-12 13:01:06 -0500

    added .gitignore/global .gitignore

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,6 +8,16 @@ public class Game {
     private String[] markers = {"X","O"};
     private Scanner playerTurn;
     private boolean gameWon = false;
+    private int[][] winningBoards = {
+            {0,1,2},
+            {3,4,5},
+            {6,7,8},
+            {0,3,6},
+            {1,4,7},
+            {2,5,8},
+            {0,4,8},
+            {2,4,6}
+    };
 
     public Game(Board board, Validation validation) {
         this.board = board;
@@ -29,7 +39,17 @@ public class Game {
     }
 
     public void victory() {
-        String[][] possiblyWins = {{this.play}}
+
+//        String[][] winningBoards = {
+//                {"1","2","3"},
+//                {"4","5","6"},
+//                {"7","8","9"},
+//                {"1","4","7"},
+//                {"2","5","8"},
+//                {"3","6","9"},
+//                {"1","5","9"},
+//                {"3","5","7"}
+//        };
     }
 
     public void playGame() {
