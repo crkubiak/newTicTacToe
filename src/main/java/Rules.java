e367c61bc8a088906218eb0ecab766e62f75f026
Charles Kubiak <crkubiak@Charless-MacBook-Pro.local>
2019-06-14 11:20:15 -0500

    testVictoryCheck passing, ready to integrate into rules

--- src/main/java/Rules.java
+++ src/main/java/Rules.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Rules {
 
-    private boolean gameWon = false;
+    private String playerWon = "";
     private Integer[] currentPlayersBoard = {0,0,0,0,0,0,0,0,0};
+    private HashSet<Integer> victorySet = new HashSet<>();
     private Integer[][] winningBoards = {
             {0,1,2},
             {3,4,5},
@@ -13,12 +17,19 @@ public class Rules {
             {2,4,6}
     };
 
-    public boolean victoryCheck(String[] board, String currentPlayer) {
+    public String victoryCheck(String[] board, String currentPlayer) {
         Integer[] boardToCheck = playerConversion(board, currentPlayer);
-
+        for (int game = 0; game < currentPlayersBoard.length - 1; game++) {
+            victorySet.addAll(Arrays.asList(boardToCheck));
+            victorySet.retainAll(Arrays.asList(winningBoards[game]));
+            if (victorySet.size() == 3) {
+                playerWon = currentPlayer;
+            }
+        }
+        return playerWon;
     }
 
-    private Integer[] playerConversion(String[] board, String currentPlayer) {
+    public Integer[] playerConversion(String[] board, String currentPlayer) {
 
         for (int i = 0; i < board.length; i++) {
             if (board[i] == currentPlayer) {
