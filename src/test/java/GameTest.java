b318187d8d2ca35a0e6b2b57d676f5a3ef7eb0af
Charles Kubiak <crkubiak@Charless-MacBook-Pro.local>
2019-06-18 15:37:58 -0500

    comment removal

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -7,7 +7,6 @@ public class GameTest {
 
     @Test
     public void gameStartsWithPlayerX() {
-
         Validation validation = new Validation();
         Board board = new Board();
         Rules rules = new Rules();
@@ -43,18 +42,9 @@ public class GameTest {
         Board board = new Board();
         Rules rules = new Rules();
         Game game = new Game(board, validation, rules);
-        game.move(4);//note
+        game.move(4);
         game.move(6);
         String[] expectedBoard = {"1","2","3","X","5","O","7","8","9"};
         assertArrayEquals(expectedBoard, board.currentMoves());
     }
-
-
-//    @Test
-//    public void testPlayGame() {
-//        Validation validation =new Validation();
-//        Board board = new Board();
-//        Game game = new Game(board, validation);
-//        game.playGame();
-//    }
 }
