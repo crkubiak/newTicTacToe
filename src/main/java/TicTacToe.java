bc5c729229ea4f3f0eb78b83ef9871aa72b5d57d
Charles Kubiak <crkubiak@Charless-MacBook-Pro.local>
2019-06-18 14:52:36 -0500

    working game

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class TicTacToe {
+
     public static void main(String[] args) {
-        Board board = new Board();
-        Validation validation = new Validation();
-        Rules rules = new Rules();
-        Game game = new Game(board, validation, rules);
-        game.playGame();
+        boolean wantsToPlay = true;
+        while (wantsToPlay){
+            System.out.print("\033[H\033[2J");
+            Board board = new Board();
+            Validation validation = new Validation();
+            Rules rules = new Rules();
+            Game game = new Game(board, validation, rules);
+            game.individualGameLoop();
+            System.out.println("Would you like to play again? (y/n): ");
+            Scanner playAgain = new Scanner(System.in);
+            wantsToPlay = playAgain.hasNext("y");
+        }
     }
 }
